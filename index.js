90160ce4fe5ded7b4a4fe4de2723ff1af7d17930
Satyam Shukla <satyamshukla.91000@gmail.com>
2024-12-03 12:17:31 +0530

    add event delegation

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("first")
+document.getElementById('child').addEventListener('click', (e) => {
+  console.log(`Child clicked from ${e.target.id}`);
+},true);
+
+document.getElementById('parent').addEventListener("click", (e) => {
+    console.log(`parent clicked from ${e.target.id}`);
+},true);
+
+document.getElementById('grantparent').addEventListener("click", (e) => {
+    console.log(`grantparent clicked from ${e.target.id}`);
+    e.stopPropagation();
+},true);
